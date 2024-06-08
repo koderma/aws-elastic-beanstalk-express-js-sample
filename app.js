8aeae358cadbb9aded384902a7aa7876cce3899b
Vikrant Kumar <vikrant.psdba@gmail.com>
2024-06-08 14:55:18 +0530

    full pipleine test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hare Krishna!'));
+app.get('/', (req, res) => res.send('Hare Krishna Pipeline test Completed!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
